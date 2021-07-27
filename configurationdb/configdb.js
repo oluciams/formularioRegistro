108d4b9ee8a02c7e9752b6f8b6198f47a034205e
olga <oluciams84@gmail.com>
2021-07-27 15:28:15 -0500

    varibles de entorno para mongoose

--- configurationdb/configdb.js
+++ configurationdb/configdb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/mongo-4', { 
+mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost:27017/mongo-4', { 
     useNewUrlParser: true,
     useUnifiedTopology: true
  });
